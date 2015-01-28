f189a903522151fff5b8090893689e361dd72265
MrFabkirox <fabrice.esope@gmail.com>
2015-01-28 16:28:40 +0000

    first commit

---
+++ src/main/java/com/proj1/proj1/App.java
@@ -0,0 +1,18 @@
+package com.proj1.proj1;
+
+import controllers.Action;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+    	
+    	Action a = new Action();
+    	
+    	a.dispValueOfText();
+    	
+    }
+}
---
+++ src/main/java/controllers/Action.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class Action {
+
+	String text = "";
+	
+	public Action() {
+		
+		this.text = ("index.html").toString();
+	}
+
+	public void dispValueOfText() {
+		System.out.println(text);
+	}
+}
